37d7ab6dfb0ef8ab7f703fca4962bdfc96361ef4
Jamie Saxon <jsaxon@uchicago.edu>
2020-12-18 16:30:48 -0600

    add python dependencies and add extra files for run script

--- aws/fetch_and_run.sh
+++ aws/fetch_and_run.sh
@@ -4,6 +4,12 @@ echo "Running" $SCRIPT $ARGS
 
 aws s3 cp $SCRIPT script
 
+if [ ! -z "$EXTRA_FILES" ]; then
+  for f in $EXTRA_FILES; do 
+    aws s3 cp $f .
+  done
+fi
+
 chmod +x script
 
 echo "Beginning execution..."
